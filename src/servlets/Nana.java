50af3b4e040b03a1058a6d108afafbe3c2981e77
s117t <s117t@JUNGTAEPC>
2021-02-24 20:01:28 +0900

    20210224/One more time commit

---
+++ src/servlets/Nana.java
@@ -0,0 +1,32 @@
+package servlets;
+
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+@WebServlet("/hi")
+public class Nana extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		resp.setCharacterEncoding("UTF-8");
+		resp.setContentType("text/html; charset=UTF-8");
+		
+		PrintWriter out = resp.getWriter();
+		
+		int cnt = 100;
+		String cnt_ = req.getParameter("cnt");
+		
+		if(!(cnt_== null)&&!(cnt_.equals(""))) 
+			cnt = Integer.parseInt(cnt_);
+		
+		for(int i = 0; i<cnt; i++) {
+			out.println((i+1)+ ": ¾È³ç Servlet!!<br/>");
+		}
+		
+	}
+
+}
